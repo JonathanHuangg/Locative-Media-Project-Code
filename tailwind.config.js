65886fded4631d3f858a925095fc1ec668b19b7d
Jonathan Huang <jonathannhuang@gmail.com>
2023-07-19 10:36:12 +0200

    tailwind in

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["/Users/jonathan/Desktop/GT Master Files/Summer 1/qWebApp/JonathanHuangg.github.io/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
